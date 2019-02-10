e0423beb04d6573920eb9dafe785c9f1f4a917a0
meteor <cherrymeteor@outlook.com>
2019-02-10 20:54:32 +0000

    pack gateway

--- run infrastructure.bat
+++ run infrastructure.bat
@@ -3,5 +3,5 @@ start cmd /K run.bat
 cd ../cloudAuth
 start cmd /K java -jar "build/libs/cloud-auth-0.0.1-SNAPSHOT.war" --spring.profiles.active=dev
 cd ../cloudGateway
-start cmd /K run.bat
+start cmd /K java -jar "build/libs/cloud-gateway-0.0.1-SNAPSHOT.war" --spring.profiles.active=dev,no-liquibase
 cd ..

36ed57f8c883193e529db197688025da6d29aaf9
meteor <cherrymeteor@outlook.com>
2019-03-01 20:13:09 +0000

    change properties of module selection

--- svcProgrammeModule/src/main/java/cc/orangejuice/srs/programme/module/client/ProgrammeFeignClient.java
+++ svcProgrammeModule/src/main/java/cc/orangejuice/srs/programme/module/client/ProgrammeFeignClient.java
@@ -1,7 +1,7 @@
 package cc.orangejuice.srs.programme.module.client;
 
 import cc.orangejuice.srs.programme.domain.enumeration.ProgrammePropType;
-import cc.orangejuice.srs.programme.service.dto.ProgrammePropDictDTO;
+import cc.orangejuice.srs.programme.service.dto.ProgrammePropDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +10,7 @@ import java.util.Optional;
 @AuthorizedFeignClient(name = "svc-programme")
 interface ProgrammeFeignClient {
     @GetMapping(value = "/api/programme-prop")
-    Optional<ProgrammePropDictDTO> getProgrammeProp(
+    Optional<ProgrammePropDTO> getProgrammeProp(
         @RequestParam(value = "type", defaultValue = "GENERAL") ProgrammePropType type,
         @RequestParam(value = "forEnrollYear", required = false) Integer forEnrollYear,
         @RequestParam(value = "forYearNo", required = false) Integer forYearNo,

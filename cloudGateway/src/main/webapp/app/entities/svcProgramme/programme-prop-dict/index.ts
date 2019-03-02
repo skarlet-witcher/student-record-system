43fa0f9a42c3bd4d4de1231773a26c4818174d39
meteor <cherrymeteor@outlook.com>
2019-03-02 14:06:16 +0000

    gateway update

--- cloudGateway/src/main/webapp/app/entities/svcProgramme/programme-prop-dict/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './programme-prop-dict.service';
-export * from './programme-prop-dict-update.component';
-export * from './programme-prop-dict-delete-dialog.component';
-export * from './programme-prop-dict-detail.component';
-export * from './programme-prop-dict.component';
-export * from './programme-prop-dict.route';
